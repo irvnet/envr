f5945d0e622a69d25ab3f3e8e208ba6aae166946
irvnet <irvnet@gmail.com>
2025-04-06 19:52:14 -0400

    add: version

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var verbose bool
+var version = "v0.1.0"
+
 func main() {
 	var rootCmd = &cobra.Command{
 		Use:   "envr",
@@ -20,13 +23,23 @@ func main() {
 	var sampleCmd = &cobra.Command{
 		Use:   "sample",
 		Short: "Sample Command",
-		Long:  "This is a sample command for sample purposes:w http.ResponseWriter, r *http.Request",
+		Long:  "This is a sample command for sample purposes",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("sample command...")
 		},
 	}
 
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Version Command",
+		Long:  "Current version for environment checker",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Environment Readiness Checker: %s\n", version)
+		},
+	}
+
 	rootCmd.AddCommand(sampleCmd)
+	rootCmd.AddCommand(versionCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
