49052010ee2ed36ee4989f2ff2a06f958dff45b1
irvnet <irvnet@gmail.com>
2025-04-06 20:23:12 -0400

    add: version

--- main.go
+++ main.go
@@ -1,26 +1,37 @@
-
 package main
 
 import (
-        "fmt"
-        "os"
+	"fmt"
+	"os"
 
-        "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
+var version = "v0.1.0"
+
 func main() {
-        var rootCmd = &cobra.Command{
-                Use:   "envr",
-                Short: "Environment Readiness Checker",
-                Long:  "Environment Readiness Checker (envr) helps you validate Linux environments for Go, Kubernetes, and vCluster development.",
-                Run: func(cmd *cobra.Command, args []string) {
-                        fmt.Println("envr: Environment Readiness Checker")
-                },
-        }
+	var rootCmd = &cobra.Command{
+		Use:   "envr",
+		Short: "Environment Readiness Checker",
+		Long:  "Environment Readiness Checker (envr) helps you validate Linux environments for Go, Kubernetes, and vCluster development.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("envr: Environment Readiness Checker")
+		},
+	}
 
-        if err := rootCmd.Execute(); err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "version",
+		Long:  "print Environment Readiness Checker version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("envr: %s\n",  version)
+		},
+	}
+
+	rootCmd.AddCommand(versionCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
-
